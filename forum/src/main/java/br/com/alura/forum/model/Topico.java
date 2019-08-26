e8851fd5f08fa98e03d79ad58b9dbe6ea206be6a
Flavio Rocha <flavio.rocha@tivit.com>
2019-08-25 23:25:07 -0300

    Implementado o CRUD da API (POST, GET, PUT e DELETE)

--- forum/src/main/java/br/com/alura/forum/model/Topico.java
+++ forum/src/main/java/br/com/alura/forum/model/Topico.java
@@ -31,6 +31,17 @@ public class Topico {
 		return result;
 	}
 
+	public Topico(){
+
+	}
+
+
+	public Topico(String titulo, String mensagem, Curso curso) {
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
